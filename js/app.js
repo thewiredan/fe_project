394c361a276d6318c80e7242908641e028883b73
Daniel <jordand1986@gmail.com>
2018-03-25 14:09:32 -0400

    fixed alignment of image overlay

--- js/app.js
+++ js/app.js
@@ -1,11 +1,9 @@
-$("figure").hover(
+$("figure").hover(                              // when hovering over a figure overlays figcaption and link to project
     function(){
-    $(this).children("figcaption").css({
-        "display": "inline-block",
-        "height": "",
-        "width": ""
-      });
+    $(this).children("figcaption").css({        // targets figcaption of figure parent to set display, height, and width
+        "display": "block",
+        "height": $(this).children("img").height(), //sets height to image height
+        "width": $(this).children("img").width(),   //sets width to image width
     }, function(){
-    $(this).children("figcaption").css("display", "none");
+    $(this).children("figcaption").css("display", "none"); //hides overlay on hover exit
 });
-
